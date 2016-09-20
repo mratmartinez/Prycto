d81fdc589ae36479ffce572ad307dded7b47085a
Juan Martínez <mratmartinez@anche.no>
2016-09-20 19:15:40 -0300

    Love

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
@@ -7,3 +8,14 @@ def index():
     url_for('static', filename='style.css')
     return rem
 
+if __name__ == '__main__':
+	app.run(port=9080)
+
+"""
+ A guy called "ThiefMaster" in an IRC Channel told me that I can run
+ the server from this main script in this way. If you are reading this:
+ 	I really love you, man.
+ Also, another one in the same channel (called "DLSteve") told me how to
+ specify the port.
+ 	I love you too, like bros love.
+"""
