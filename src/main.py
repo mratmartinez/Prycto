375cdd04dd85350516c86a86b9c9c8ea99febfdb
Juan Martínez <mratmartinez@anche.no>
2016-09-20 17:10:23 -0300

    Ahí me va gustando

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    rem = render_template('main.html')
+    rem = render_template('index.html')
     url_for('static', filename='style.css') 
     return rem
 
